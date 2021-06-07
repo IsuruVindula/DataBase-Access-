4ac5f2f0c3cc1e04078bfb3c7203d415c28fca45
Isuru_Vindula <isuru.vindula@axiatadigitallabs.com>
2021-06-07 12:02:29 +0530

    before adding redux

--- src/components/all_table/Source.js
+++ src/components/all_table/Source.js
@@ -1,6 +1,8 @@
 import { Container, Label} from "reactstrap";
 import { Link } from "react-router-dom";
 import Tabledetails from './Tabledetails';
+import { useState } from "react";
+
 
 
 const DataList = ({datalist, handleDelete, handleEdit}) => {
@@ -8,12 +10,20 @@ const DataList = ({datalist, handleDelete, handleEdit}) => {
 
   console.log(datalist)
   let numarray = Object.keys(datalist);
-  console.log(numarray)
+  console.log("numarray",numarray)
   let keyarray = numarray.map(i => {return(Object.keys(datalist[i]))})
+  let tables = [];
+
+  const [clicked, setclicked] = useState(false);
 
 
-const databaseSelect = (databasename) => {
-  <Tabledetails/>
+const databaseSelect = async (databasename) => {
+  // console.log("database name",datalist[0]['ti_fact_exec_customer_tt_d']);
+  setclicked(false);
+  // const source = datalist.filter(data => {return (Object.keys(datalist))})
+  Object.keys(datalist).forEach((data) => {if (typeof datalist[data][databasename] !== 'undefined'){tables.push(datalist[data][databasename])}})
+  console.log(databasename, tables);
+  setclicked(true);
 }
 
   return(
@@ -27,12 +37,11 @@ const databaseSelect = (databasename) => {
       <div>
         <ul>
           {
-            keyarray.map((item,i) => {return (<li key={i}><button>{item}</button></li>)})
-            {/* keyarray.map((item,i) => {return (<li key={i}><Link onClick={()=>{databaseSelect()}} to=''>{item}</Link></li>)}) */}
-
+            keyarray.map((item,i) => {return (<li key={i}><button onClick={()=>{databaseSelect(item)}}>{item}</button></li>)})
           }
         </ul>
       </div>
+      {tables.length > 0 && clicked ? <Tabledetails data={tables}/> : <div></div>}
       </Container>
     )
 }
--- src/components/all_table/Tabledetails.jsx
+++ src/components/all_table/Tabledetails.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function Tabledetails(){
+function Tabledetails(data){
 
     // const {tablename} = useParams();
+    console.log(data);
 
     return (
         <div>
-            <h2>Helloooooooooooooooooooooooooo</h2>
+            <h2>Hoooooooooooooooooooooooooooooo</h2>
         </div>
     )
 }
